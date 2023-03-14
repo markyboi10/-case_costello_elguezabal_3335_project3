189edcb48ad6eca352b4d731addc87703419b445
Mark Case <casem@merrimack.edu>
2023-03-14 10:25:49 -0400

    Working version of stuetzle code

--- src/csc3335_project3/Agent_case_costello_elguezabal.java
+++ src/csc3335_project3/Agent_case_costello_elguezabal.java
@@ -11,4 +11,9 @@ import java.util.Random;
  */
 public class Agent_case_costello_elguezabal implements GipfPlayable {
 
+    @Override
+    public String makeGipfMove(int curPlayer) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
 }

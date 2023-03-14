9e0db76f21e76a8f8ac9cd6e9dabbf3e8553cb97
Mark Case <casem@merrimack.edu>
2023-03-13 20:38:19 -0400

    Project Creation

---
+++ src/gipf/Gipf.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gipf;
+
+/**
+ *
+ * @author Mark Case
+ */
+public class Gipf {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

3111b5fa6396c2bb83b4793a474d8310cfffcb91
Mark Case <casem@merrimack.edu>
2023-03-14 10:23:21 -0400

    Adding Stuetzle code and creating our agent class

---
+++ src/csc3335_project3/Agent_case_costello_elguezabal.java
@@ -0,0 +1,14 @@
+package csc3335_project3;
+
+import csc3335.gipf_game.GipfGame;
+import csc3335.gipf_game.GipfPlayable;
+import java.time.Instant;
+import java.util.Random;
+
+/**
+ *
+ * @author Mark, Alex, Derek
+ */
+public class Agent_case_costello_elguezabal implements GipfPlayable {
+
+}
